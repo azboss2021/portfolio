186114ca100b88da14692cf5102c7d8a448ba60b
Caleb Wilson <alexanderzyn1@gmail.com>
2025-05-10 03:17:00 -0400

    Update styles, no more shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,10 @@
 import type { Metadata } from "next";
-import { Merriweather, Montserrat, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "react-hot-toast";
-import { ThemeProvider } from "@/components/ThemeProvider";
 
 const poppins = Poppins({
 	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 	subsets: ["devanagari", "latin", "latin-ext"],
-});
-const merriweather = Merriweather({
-	weight: ["300", "400", "700", "900"],
-	subsets: ["cyrillic", "cyrillic-ext", "latin", "latin-ext", "vietnamese"],
-});
-const montserrat = Montserrat({
-	subsets: ["cyrillic"],
 });
 
 export const metadata: Metadata = {
@@ -28,16 +19,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" suppressHydrationWarning>
-			<body className={montserrat.className}>
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="dark"
-					disableTransitionOnChange
-				>
-					<Toaster />
-					{children}
-				</ThemeProvider>
-			</body>
+			<body className={`${poppins.className}`}>{children}</body>
 		</html>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import EmailForm from "@/components/EmailForm";
-import ThemeToggle from "@/components/ThemeToggle";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
 import {
 	FaGraduationCap,
@@ -9,6 +6,8 @@ import {
 	FaEye,
 	FaGithub,
 	FaLinkedin,
+	FaEnvelope,
+	FaBriefcase,
 } from "react-icons/fa6";
 
 const projects: {
@@ -18,40 +17,34 @@ const projects: {
 	repoLink?: string;
 	liveDemoLink?: string;
 	demoLink: string;
-	skills: { skill: string; text?: string; color?: string }[];
+	skills: string[];
 }[] = [
 	{
 		title: "CaptionIt.fun",
 		date: "2024",
 		description:
-			"This is my first full stack app that I built using my Next.js boilerplate. It accepts payments and is user ready. As of 4/5/24, there is lots of work to be done, and bugs to be fixed. Until then, I will be periodically updating it as I work on other projects.",
+			"This is my first full stack app that I built using my Next.js boilerplate. It accepts payments and is user ready. Update (2025): The application is no longer deployed, but feel free to check out the demo video to see how it operated.",
+		repoLink: "https://github.com/azboss2021/captionit",
 		demoLink: "https://www.youtube.com/embed/9DapZ_-stPY?si=vOFsuNWlBbiKJYRx",
-		liveDemoLink: "https://www.captionit.fun",
 		skills: [
-			{ skill: "Next.js" },
-			{ skill: "React" },
-			{ skill: "Typescript" },
-			{ skill: "Tailwind" },
-			{ skill: "Stripe" },
-			{ skill: "MongoDB" },
-			{ skill: "Cloudinary" },
-			{ skill: "OpenAI" },
+			"Next.js",
+			"React",
+			"Typescript",
+			"Tailwind",
+			"Stripe",
+			"MongoDB",
+			"Cloudinary",
+			"OpenAI",
 		],
 	},
 	{
-		title: "Next.js Full Stack Boilerplate",
+		title: "Next.js Full Stack SaaS Boilerplate",
 		date: "2024",
 		description:
 			"This boilerplate built over a few weeks encompasses most of the tools I would use to build a SaaS. It has an authentication system, account actions, support email page, stripe payments, newsletter subscription, and a customizeable landing page.",
+		repoLink: "https://github.com/azboss2021/my-saas",
 		demoLink: "https://www.youtube.com/embed/U_4kkuXUN_0?si=QATweDpnznBj1RWs",
-		skills: [
-			{ skill: "Next.js" },
-			{ skill: "React" },
-			{ skill: "Typescript" },
-			{ skill: "Tailwind" },
-			{ skill: "Stripe" },
-			{ skill: "MongoDB" },
-		],
+		skills: ["Next.js", "React", "Typescript", "Tailwind", "Stripe", "MongoDB"],
 	},
 	{
 		title: "Pathfinding Visualizer",
@@ -61,12 +54,7 @@ const projects: {
 		repoLink: "https://github.com/azboss2021/pathfinding",
 		liveDemoLink: "https://azboss2021.github.io/pathfinding/",
 		demoLink: "https://www.youtube.com/embed/73MtV735ssQ?si=Y6g7Fp7PJgGP6EJS",
-		skills: [
-			{ skill: "Vite" },
-			{ skill: "React" },
-			{ skill: "Javascript" },
-			{ skill: "Tailwind" },
-		],
+		skills: ["Vite", "React", "Javascript", "Tailwind"],
 	},
 	{
 		title: "Sorting Visualizer",
@@ -76,76 +64,77 @@ const projects: {
 		repoLink: "https://github.com/azboss2021/sorting-practice",
 		liveDemoLink: "https://azboss2021.github.io/sorting-practice/",
 		demoLink: "https://www.youtube.com/embed/SBSD4NFydFY?si=eQr-JtcSV-ABaSsP",
-		skills: [
-			{ skill: "Vite" },
-			{ skill: "React" },
-			{ skill: "Javascript" },
-			{ skill: "Tailwind" },
-		],
-	},
-	{
-		title: "CV Builder",
-		date: "2022",
-		description:
-			"This was one of my first projects that I made while learning React states and forms. It is included in this project list to show my progress while self-learning.",
-		repoLink: "https://github.com/azboss2021/cv-project",
-		liveDemoLink: "https://azboss2021.github.io/cv-project/",
-		demoLink: "https://www.youtube.com/embed/LSeUnKWt6XI?si=fZ19bHORLwRCRnAV",
-		skills: [
-			{ skill: "Create React App" },
-			{ skill: "React" },
-			{ skill: "Javascript" },
-		],
+		skills: ["Vite", "React", "Javascript", "Tailwind"],
 	},
 ];
 
 export default function Home() {
 	return (
 		<main className="flex flex-col items-center pt-20 pb-32 gap-24 max-w-5xl mx-auto px-8">
-			<ThemeToggle />
-			<div className="text-center flex flex-col gap-1 items-center pb-16 border-b w-full">
-				<h1 className="text-2xl">Caleb Wilson</h1>
-				{/* <span className="opacity-80">Full Stack Dev</span> */}
-				<div className="flex flex-col sm:flex-row md:gap-2 gap-1 items-center text-sm">
-					<Link
-						href="mailto:cwilsonfun@gmail.com"
-						target="_blank"
-						className="hover:underline"
-					>
-						cwilsonfun@gmail.com
-					</Link>
-					<span className="flex items-center gap-0.5 text-sm">
-						<FaLocationDot size={14} /> Las Vegas, NV
-					</span>
-					<span className="flex items-center gap-0.5 text-sm">
-						<FaGraduationCap size={14} /> UNLV CS 2023
-					</span>
-				</div>
-				<div className="flex gap-1.5">
-					<Button
-						asChild
-						className="w-8 h-8 p-0 rounded-full"
-						variant="default"
-					>
-						<Link href="https://github.com/azboss2021" target="_blank">
-							<FaGithub size={18} />
-							<span className="sr-only">GitHub Link</span>
+			<div className="flex items-start justify-between w-full">
+				<div className="flex flex-col gap-4 items-start">
+					<h1 className="text-6xl font-bold">Caleb Wilson</h1>
+
+					<h2 className="text-2xl">Full Stack & Next.js Developer</h2>
+
+					<div className="flex flex-col gap-2 items-start opacity-80">
+						<Link
+							href="mailto:calebjwilson14@gmail.com"
+							target="_blank"
+							className="hover:underline flex items-start gap-2"
+						>
+							<div className="mt-1">
+								<FaEnvelope />
+							</div>
+							calebjwilson14@gmail.com
 						</Link>
-					</Button>
-					<Button
-						asChild
-						className="w-8 h-8 p-0 rounded-full"
-						variant="default"
-					>
 						<Link
 							href="https://www.linkedin.com/in/calebwilson-lv/"
 							target="_blank"
+							className="hover:underline flex items-start gap-2"
 						>
-							<FaLinkedin size={18} />
-							<span className="sr-only">LinkedIn Link</span>
+							<div className="mt-1">
+								<FaLinkedin />
+							</div>
+							calebwilson-lv
 						</Link>
-					</Button>
+						<Link
+							href="https://github.com/azboss2021"
+							target="_blank"
+							className="hover:underline flex items-start gap-2"
+						>
+							<div className="mt-1">
+								<FaGithub />
+							</div>
+							azboss2021
+						</Link>
+						<div className="flex items-start gap-2">
+							<div className="mt-1">
+								<FaBriefcase size={14} />
+							</div>
+							General Dynamics (2024 - 2025)
+						</div>
+						<div className="flex items-start gap-2">
+							<div className="mt-1">
+								<FaGraduationCap size={14} />
+							</div>
+							BSc Computer Science @ UNLV (2023)
+						</div>
+						<div className="flex items-start gap-2">
+							<div className="mt-1">
+								<FaLocationDot size={14} />
+							</div>
+							Moon Township, PA
+						</div>
+					</div>
 				</div>
+				<Image
+					width={196}
+					height={196}
+					src="/CJ.jpeg"
+					alt="CJ"
+					className="rounded-full"
+				/>
 			</div>
 
 			<section className="flex flex-col gap-24">
@@ -155,47 +144,40 @@ export default function Home() {
 						className="flex flex-col md:flex-row gap-4 md:gap-12"
 					>
 						<div className="flex flex-col gap-2 basis-1/2">
-							<div className="flex flex-col gap-2 justify-center h-full items-center md:items-start">
-								<h2 className="font-extrabold text-2xl items-center text-center md:text-left">
-									{project.title}{" "}
-									<span className="font-normal">
-										{project.date && <>({project.date})</>}
-									</span>
+							<div className="flex flex-col gap-4 justify-center h-full items-start">
+								<h2 className="text-2xl items-center font-bold text-left">
+									{project.title}
 								</h2>
-								<h3 className="text-center md:text-left max-w-prose">
-									{project.description}
-								</h3>
-								<div className="flex flex-wrap gap-1 sm:justify-start justify-center">
+								<h3 className="text-left max-w-prose">{project.description}</h3>
+								<div className="flex flex-wrap gap-1 justify-start">
 									{project.skills.map((skill, index) => (
-										<Badge
-											key={`${project.title}_skill_${index}`}
-											variant="outline"
+										<div
+											key={index}
+											className="px-1.5 py-0.5 bg-black/10 rounded"
 										>
-											{skill.skill}
-										</Badge>
+											{skill}
+										</div>
 									))}
 								</div>
 								{(project.repoLink || project.liveDemoLink) && (
 									<div className="flex w-full gap-1">
 										{project.repoLink && (
-											<Button
-												asChild
-												className="w-full md:w-fit flex items-center gap-2"
+											<Link
+												href={project.repoLink}
+												target="_blank"
+												className="bg-black text-white px-3 py-1.5 flex items-center gap-2 rounded hover:opacity-60 transition-opacity duration-200"
 											>
-												<Link href={project.repoLink} target="_blank">
-													Code <FaGithub size={16} />
-												</Link>
-											</Button>
+												Code <FaGithub size={16} />
+											</Link>
 										)}
 										{project.liveDemoLink && (
-											<Button
-												asChild
-												className="w-full md:w-fit flex items-center gap-2 bg-gradient-to-r from-purple-500 to-pink-500 hover:opacity-90 transition-opacity text-white"
+											<Link
+												href={project.liveDemoLink}
+												target="_blank"
+												className="bg-black text-white px-3 py-1.5 flex items-center gap-2 rounded hover:opacity-60 transition-opacity duration-200"
 											>
-												<Link href={project.liveDemoLink} target="_blank">
-													Demo <FaEye size={16} />
-												</Link>
-											</Button>
+												Demo <FaEye size={16} />
+											</Link>
 										)}
 									</div>
 								)}
@@ -205,14 +187,12 @@ export default function Home() {
 							src={`${project.demoLink}?disablekb=0&rel=0`}
 							title="YouTube video player"
 							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-							className="basis-1/2 aspect-video"
+							className="basis-1/2 aspect-video rounded"
 							allowFullScreen
 						/>
 					</div>
 				))}
 			</section>
-
-			<EmailForm />
 		</main>
 	);
 }
